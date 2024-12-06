6b4974ce776bec878e7eb496c310d12c99cdef29
bjackowitz-caring <127962055+bjackowitz-caring@users.noreply.github.com>
2024-12-06 15:40:01 -0500

    Updated models for Order items and products

--- lessons/models/staging/stg_ecommerce__order_items.sql
+++ lessons/models/staging/stg_ecommerce__order_items.sql
@@ -7,19 +7,30 @@ WITH source as (
 
 SELECT
 	-- IDs
-	id as order_item_id,
+	id AS order_item_id,
 	order_id,
 	user_id,
 	product_id,
-	inventory_item_id,
 
-	-- Datetimes
-	created_at,
-	delivered_at,
-	returned_at,
+	-- Other columns
+	sale_price AS item_sale_price
 
-	-- Order details
-	status,
-	sale_price
+	{# Unused Columns
+
+		- inventory_item_id
+
+		For the below, we assume that all of these will be the same
+		in stg_ecommerce__orders so we can use that as the source of truth
+		(e.g. a whole order is returned, not just 1 item).
+		This is a simplification for the purpose of this course.
+		- gender
+		- status
+		- created_at
+		- shipped_at
+		- delivered_at
+		- returned_at
+
+	#}
+
 FROM
 	source
--- lessons/models/staging/stg_ecommerce__products.sql
+++ lessons/models/staging/stg_ecommerce__products.sql
@@ -6,15 +6,24 @@ WITH source as (
 SELECT
 	-- IDs
 	id as product_id,
-	sku as product_sku,
-	distribution_center_id
+
 
 	-- Product details
-	name as product_name,
+
 	brand as product_brand,
 	retail_price,
-	category as product_category,
 	cost as product_cost,
 	department as product_department,
+
+	{#Unused Columns:
+
+	- sku
+	- distribution_center_id
+	- name
+	- inventory_item_id
+	- category
+
+	#}
+
 FROM
 	source
