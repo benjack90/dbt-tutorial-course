31e4f1db55e73dec0b873753e72293d1d7317f08
bjackowitz-caring <127962055+bjackowitz-caring@users.noreply.github.com>
2024-12-07 15:06:12 -0500

    Updated tests for staging models and created intermediate model file.

---
+++ lessons/models/intermediate/int_ecommerce__order_items_products.sql
@@ -0,0 +1,30 @@
+with products as (
+  SELECT
+    product_id,
+	product_department,
+    product_retail_price,
+	product_cost
+  FROM
+    {{ ref('stg_ecommerce__products')}}
+
+)
+
+SELECT
+
+	oi.order_item_id,
+	oi.order_id,
+	oi.user_id,
+	oi.product_id,
+
+	oi.item_sale_price,
+
+	p.product_department,
+	p.product_retail_price,
+	p.product_cost,
+
+	oi.item_sale_price - p.product_cost as item_profit,
+	p.product_retail_price - oi.item_sale_price as item_discount
+
+FROM
+	{{ ref('stg_ecommerce__order_items') }} oi
+	LEFT JOIN products p on p.product_id = oi.product_id
